861d880a06ab1e7a70a8b82714abdd1f81690e89
Evelina Bragsjö <49522879+evelinaBragsjo@users.noreply.github.com>
2020-11-27 16:15:22 +0100

    Steg3 (First try on todo-API).

--- lib/model.dart
+++ lib/model.dart
@@ -1,49 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:TodoApp/TodoApi.dart';
 
 
 class TodoRow{
+  String id;
   String todoText;
   bool completed;
 
-  TodoRow({this.todoText, this.completed = false});
+  TodoRow({this.id, this.todoText, this.completed = false});
 
   void completedTodo(){
     completed =! completed;
   }
+
+  static Map<String, dynamic> toJson(TodoRow todoRow) {
+    return{
+      'title': todoRow.todoText,
+      'done': todoRow.completed,
+    };
+  }
+
+  static TodoRow fromJson(Map<String, dynamic> json) {
+    return TodoRow(
+      id: json['id'],
+      todoText: json['title'],
+      completed: json['done'], 
+    );
+  }
 }
+
 
 class MyState extends ChangeNotifier{
   List<TodoRow> _list = [];
-
   String _filterBy = 'all';
 
   List<TodoRow> get list => _list;
 
   String get filterBy => _filterBy;
 
-  void addTodo(TodoRow todoRow){
-    _list.add(todoRow);
+  Future getList() async {
+    List<TodoRow> list = await TodoApi.getTodos();
+    _list = list;
     notifyListeners();
-
+  }
+  
+  void addTodo(TodoRow todoRow) async {
+    await TodoApi.addTodo(todoRow);
+    await getList();
+    notifyListeners();
   }
 
-  void removeTodo(TodoRow todoRow){
-    _list.remove(todoRow);
-    notifyListeners();
+  void removeTodo(TodoRow todoRow) async {
+    await TodoApi.deleteTodo(todoRow.id);
+    await getList();
   }
 
   void changeTodo(TodoRow todoRow){
-  final todoRowIndex = _list.indexOf(todoRow);
-  _list[todoRowIndex].completedTodo();
-  notifyListeners();
+    final todoRowIndex = _list.indexOf(todoRow);
+    _list[todoRowIndex].completedTodo();
+    notifyListeners();
   }
 
-  void setFilterBy(String filterBy) {
+  void filterTodos(String filterBy) {
     this._filterBy = filterBy;
     notifyListeners();
   }
 }
-
-//Lägg till checkboxvärde här 1. i Första klassen + konstruktor
-//2. Lägg till kompleted task beteende här som addTodo
-//3. Lägg till removeItem här också
